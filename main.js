0ab6f77e2453dce129475430b3171e37cc80e4cb
Amine <amenaymane0@gmail.com>
2022-02-19 13:32:15 +0100

    make the website phone friendly

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 let button = document.getElementById("btn");
-let menu = document.getElementById("menu")
+let menu = document.getElementById("menu");
 
-button.addEventListener("click", (event) => {
-    if (menu.style.display == "none") {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
-})
+button.addEventListener("click", () => {
+  if (menu.style.display == "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+});
+
+let header = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 70) {
+    header.classList.add("bg_header");
+  } else {
+    header.classList.remove("bg_header");
+  }
+});
